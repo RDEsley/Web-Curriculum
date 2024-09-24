d669f6b0f75b4a5161d2936f01907c2ed253680f
Richard Esley <richardesleyso@gmail.com>
2024-09-24 14:23:13 -0300

    Update Home Page CSS

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent { }
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.scrollToSection();
+  }
+
+  scrollToSection() {
+    const links = document.querySelectorAll('a.nav-link');
+    links.forEach(link => {
+      link.addEventListener('click', (event) => {
+        event.preventDefault();
+        const targetId = (link.getAttribute('href') as string).substring(1);
+        const targetElement = document.getElementById(targetId);
+        if (targetElement) {
+          window.scrollTo({
+            top: targetElement.offsetTop - 50, // Ajuste o valor conforme necessário
+            behavior: 'smooth'
+          });
+        }
+      });
+    });
+  }
+}
